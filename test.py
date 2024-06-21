eec3e3ba76645ebfc2de9a13a4dcaa481421d13a
Tan Tan <21e1020007@hueuni.edu.vn>
2024-06-21 17:28:40 +0700

    1

--- test.py
+++ test.py
@@ -10,34 +10,63 @@ df.insert(3,'wating',df['headcount'],True)
 
 # print(df)
 
-def check_wip(num_seq):
+def update_sequence(point_check):
+    sequence=point_check[1]
+    sew_collect=point_check[2]
+    df['sewing'][sequence-1]-=sew_collect
+    df['wating'][sequence-1]+=sew_collect
+    df['WIP'][sequence-1]+=sew_collect
+    
+def cal_time_check(time,num_seq,sew,list_point_check):
+    output=[]
+    point_time=time + df['SAM'][num_seq]
+    output.append(point_time)
+    output.append(num_seq+1)
+    output.append(sew)
+    list_point_check.append(output)
+    list_point_check.sort()
+    
+def cal_wip(num_seq,time,list_point_check):
     # num_seq=i ~ row=i-1
-    if df['wating'][num_seq] !=0 and df['WIP'][num_seq-1] >0 and df['WIP'][num_seq-1] <= df['wating'][num_seq]:
+    if df['wating'][num_seq] !=0 and df['WIP'][num_seq-1] ==0 and df['WIP'][num_seq-1] <= df['wating'][num_seq]:
+        cal_time_check(time,num_seq,df['WIP'][num_seq-1],list_point_check)
         df['wating'][num_seq]-=df['WIP'][num_seq-1]
         df['sewing'][num_seq]+=df['WIP'][num_seq-1]
         df['WIP'][num_seq-1]=0
+        
     elif df['wating'][num_seq] !=0 and df['WIP'][num_seq-1] > df['wating'][num_seq]:
+        cal_time_check(time,num_seq,df['wating'][num_seq],list_point_check)
         df['WIP'][num_seq-1]-=df['wating'][num_seq]
         df['wating'][num_seq]=0
         df['sewing'][num_seq]=df['headcount'][num_seq]
+def sequence_firt(time,list_point_check):
+    df['wating'][0]=0
+    df['sewing'][0]=4
+    if (time%df['SAM'][0]==0):
+        cal_time_check(time,0,4,list_point_check)
+    if (time%df['SAM'][0]==0 and time >0):
+        df['WIP'][0]+=4
 
-list_point_check=[]  
-# print(len(list_point_check)   )  
-def cal_time_check(time,num_seq):
-    point_time=time + df['SAM'][num_seq]
-    list_point_check.append(point_time)
-    list_point_check.sort()
-    return list_point_check
 
-
-def node():
-    pass
-# for i in range(1,df.shape[0]):
-#     # check_wip(i)
-time=60
-
-if len(list_point_check)!=0:
-    time_check=list_point_check.pop(0)
-    # check_wip(i)
-    pass
-# print(df)
+if __name__=="__main__":
+    list_check=[]
+    time_step=0
+    time_line=3.6
+    for i in range(1,df.shape[0]):
+        cal_wip(i,time_step,list_check)
+    sequence_firt(time_step,list_check)
+    pcheck=list_check.pop(0)
+    time_step=pcheck[0]
+    while time_step<= time_line:
+        update_sequence(pcheck)
+        for i in range(1,df.shape[0]):
+            cal_wip(i,time_step,list_check)
+        sequence_firt(time_step,list_check)
+        # print(time_step)
+        # print(df)
+        try:
+            pcheck=list_check.pop(0)
+            time_step=pcheck[0]
+        except:
+            break
+print(df)
