654a8365c1076b9931e77741af7725a2408dd9dd
Tan Tan <21e1020007@hueuni.edu.com>
2024-06-24 00:35:34 +0700

    Update depend_first

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 # import pandas lib as pd
 import pandas as pd
-
+import numpy as np
 # read by default 1st sheet of an excel file
 df= pd.read_excel('datatest.xlsx',header=0,sheet_name="Style1")
 row_name=df.columns.values
@@ -19,26 +19,26 @@ def update_sequence(point_check):
     
 def cal_time_check(time,num_seq,sew,list_point_check):
     output=[]
-    point_time=time + df.loc[num_seq,'SAM']
+    point_time=time + df.loc[num_seq-1,'SAM']
     output.append(round(point_time,1))
-    output.append(num_seq+1)
+    output.append(num_seq)
     output.append(sew)
     list_point_check.append(output)
     list_point_check.sort()
     
 def cal_wip(num_seq,time,list_point_check):
     # num_seq=i ~ row=i-1
-    if df.loc[num_seq,'wating'] > 0 and df.loc[num_seq-1,'WIP'] > 0 and df.loc[num_seq-1,'WIP'] <= df.loc[num_seq,'wating']:
-        cal_time_check(time,num_seq,df.loc[num_seq-1,'WIP'],list_point_check)
-        df.loc[num_seq,'wating']-=df.loc[num_seq-1,'WIP']
-        df.loc[num_seq,'sewing']+=df.loc[num_seq-1,'WIP']
-        df.loc[num_seq-1,'WIP']=0
+    if df.loc[num_seq-1,'wating'] > 0 and df.loc[num_seq-2,'WIP'] > 0 and df.loc[num_seq-2,'WIP'] <= df.loc[num_seq-1,'wating']:
+        cal_time_check(time,num_seq,df.loc[num_seq-2,'WIP'],list_point_check)
+        df.loc[num_seq-1,'wating']-=df.loc[num_seq-2,'WIP']
+        df.loc[num_seq-1,'sewing']+=df.loc[num_seq-2,'WIP']
+        df.loc[num_seq-2,'WIP']=0
         
-    if df.loc[num_seq,'wating']> 0 and df.loc[num_seq-1,'WIP']> df.loc[num_seq,'wating']:
-        cal_time_check(time,num_seq,df.loc[num_seq,'wating'],list_point_check)
-        df.loc[num_seq-1,'WIP']-=df.loc[num_seq,'wating']
-        df.loc[num_seq,'wating']=0
-        df.loc[num_seq,'sewing']=df.loc[num_seq,'headcount']
+    if df.loc[num_seq-1,'wating']> 0 and df.loc[num_seq-2,'WIP']> df.loc[num_seq-1,'wating']:
+        cal_time_check(time,num_seq,df.loc[num_seq-1,'wating'],list_point_check)
+        df.loc[num_seq-2,'WIP']-=df.loc[num_seq-1,'wating']
+        df.loc[num_seq-1,'wating']=0
+        df.loc[num_seq-1,'sewing']=df.loc[num_seq-1,'headcount']
      
 def sequence_first(time_st,time_li,list_point_check):
     df.loc[0,'wating']=0
@@ -46,7 +46,7 @@ def sequence_first(time_st,time_li,list_point_check):
     cycle=time_li//df.loc[0,'SAM']
     if time_st==0:
         for cyc in range(0,int(cycle)):
-            cal_time_check(cyc*df.loc[0,'SAM'],0,4,list_point_check)
+            cal_time_check(cyc*df.loc[0,'SAM'],1,4,list_point_check)
             # print(time)
             # print(".")
         
@@ -60,9 +60,9 @@ if __name__=="__main__":
     list_check=[]
     time_step=0
     time_line=100
-    for i in range(1,df.shape[0]):
-        cal_wip(i,time_step,list_check)
     sequence_first(time_step,time_line,list_check)
+    for i in range(2,df.shape[0]+1):
+        cal_wip(i,time_step,list_check)
     # print(time_step)
     # print(df)
     # for _ in list_check:
@@ -75,7 +75,7 @@ if __name__=="__main__":
         update_sequence(pcheck)
         sequence_first(time_step,time_line,list_check)
         
-        for i in range(1,df.shape[0]):
+        for i in range(2,df.shape[0]+1):
             cal_wip(i,time_step,list_check)
         # print(time_step)
         # print(df)
